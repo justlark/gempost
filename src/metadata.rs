48147229c93e44fdd38ea59d5ed56966de12dd0e
Lark <lark@lark.gay>
2024-01-12 08:40:35 -0500

    Add validation for post metadata

--- src/metadata.rs
+++ src/metadata.rs
@@ -1,8 +1,10 @@
 use std::fs::File;
 use std::path::Path;
 
+use chrono::DateTime;
 use eyre::bail;
 use serde::{Deserialize, Serialize};
+use url::Url;
 
 use crate::error::Error;
 
@@ -26,29 +28,60 @@ pub struct EntryMetadata {
     pub draft: bool,
 }
 
+fn is_uri(s: &str) -> bool {
+    Url::parse(s).is_ok()
+}
+
+fn is_rfc3339(s: &str) -> bool {
+    DateTime::parse_from_rfc3339(s).is_ok()
+}
+
+// This example comes from the Go standard library.
+const EXAMPLE_RFC3339: &str = "2006-01-02T15:04:05Z07:00";
+
 impl EntryMetadata {
     pub fn read(path: &Path) -> eyre::Result<Self> {
         let metadata_file = File::open(path)?;
 
-        match serde_yaml::from_reader(metadata_file) {
-            Ok(config) => Ok(config),
+        let metadata: Self = match serde_yaml::from_reader(metadata_file) {
+            Ok(config) => config,
             Err(err) => bail!(Error::InvalidMetadataFile {
                 path: path.to_owned(),
                 reason: err.to_string(),
             }),
-        }
-    }
-}
+        };
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct FeedMetadata {
-    pub id: String,
-    pub title: String,
-    pub page_uri: String,
-    pub feed_uri: String,
-    pub subtitle: Option<String>,
-    pub rights: Option<String>,
-    pub author: Option<AuthorMetadata>,
-    pub updated: String,
-    pub entries: Vec<EntryMetadata>,
+        metadata.validate(path)?;
+
+        Ok(metadata)
+    }
+
+    fn validate(&self, path: &Path) -> eyre::Result<()> {
+        if !is_uri(&self.id) {
+            bail!(Error::InvalidMetadataFile {
+                path: path.to_owned(),
+                reason: String::from("The post `id` must be a valid URI."),
+            })
+        }
+
+        if !is_rfc3339(&self.updated) {
+            bail!(Error::InvalidMetadataFile {
+                path: path.to_owned(),
+                reason: format!(
+                    "The post `updated` time must be in RFC3339 format (e.g. {EXAMPLE_RFC3339})."
+                ),
+            })
+        }
+
+        if let Some(published) = &self.published {
+            if !is_rfc3339(published) {
+                bail!(Error::InvalidMetadataFile {
+                    path: path.to_owned(),
+                    reason: format!("The post `published` time must be in RFC3339 format (e.g. {EXAMPLE_RFC3339})."),
+                })
+            }
+        }
+
+        Ok(())
+    }
 }
