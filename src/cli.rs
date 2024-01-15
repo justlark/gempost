31e028f03a473cb667a56e5c00e7a5cad8121e45
Lark <lark@lark.gay>
2024-01-15 13:52:22 -0500

    Organize CLI into subcommands

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,29 @@
 use std::path::PathBuf;
 
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser, Clone)]
 #[command(author, version, about)]
 pub struct Cli {
-    /// The path of the config file.
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Args, Clone)]
+pub struct Init;
+
+#[derive(Args, Clone)]
+pub struct Build {
+    /// The path of the gempost config file.
     #[arg(long, value_name = "PATH", default_value = "./gempost.yaml")]
     pub config_file: PathBuf,
+}
+
+#[derive(Subcommand, Clone)]
+pub enum Commands {
+    /// Initialize a new capsule with example templates.
+    Init(Init),
+
+    /// Build your capsule.
+    Build(Build),
 }
